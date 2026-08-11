86fe34e68058a3fa9e2c8b38b1256b1b89c532a8
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-08-11 14:04:05 +0200

    feat: add main layout and routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Sidebar } from './layout/sidebar/sidebar';
-import { Topbar } from './layout/topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, Topbar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
