f36e88f3103cb5b0c77b868fc28c352eef4369cd
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-08-11 12:54:33 +0200

    feat: add topbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './layout/sidebar/sidebar';
+import { Topbar } from './layout/topbar/topbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, Sidebar, Topbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
