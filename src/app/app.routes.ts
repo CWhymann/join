f25ba257ba32023354d20817440ffd2098ec5f93
DaKo1312 <korbmacher.d@outlook.de>
2026-08-13 12:41:45 +0200

    feat: add polics page with placeholder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainLayout } from './layout/main-layout/main-layout';
 import { Contacts } from './components/contacts/contacts';
 import { Help } from './pages/help/help';
+import { Policy } from './pages/policy/policy';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
       { path: '', pathMatch: 'full', redirectTo: 'contacts' },
       { path: 'contacts', component: Contacts },
       { path: 'help', component: Help },
+      { path: 'privacy-policy', component: Policy },
     ],
   },
   { path: '**', redirectTo: 'contacts' },
