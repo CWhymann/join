3b8ad00454819d76a436af1b8d2d7701843ff6c4
DaKo1312 <korbmacher.d@outlook.de>
2026-08-19 15:00:54 +0200

    feat: add-task main fields

--- src/app/components/add-task/add-task-form/add-task-form.ts
+++ src/app/components/add-task/add-task-form/add-task-form.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-add-task-form',
     standalone: true,
+    imports: [ReactiveFormsModule],
     templateUrl: './add-task-form.html',
     styleUrl: './add-task-form.scss',
 })
-export class AddTaskForm {}
+export class AddTaskForm {
+    private formBuilder = inject(FormBuilder);
+
+    protected readonly form = this.formBuilder.group({
+        title: [''],
+        description: [''],
+        dueDate: [''],
+    });
+}
