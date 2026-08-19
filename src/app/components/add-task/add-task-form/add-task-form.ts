479ed2196e371a1738b2f74b42a5d04ae57b267f
DaKo1312 <korbmacher.d@outlook.de>
2026-08-19 13:13:17 +0200

    feat: add-task component scaffold

---
+++ src/app/components/add-task/add-task-form/add-task-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-add-task-form',
+    standalone: true,
+    templateUrl: './add-task-form.html',
+    styleUrl: './add-task-form.scss',
+})
+export class AddTaskForm {}
