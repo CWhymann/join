91e77663aee82d6873af349de99355b415200f4f
Christian <cwiemann39@gmial.com>
2026-08-25 11:43:06 +0200

    Add TaskToastService for task created/deleted/locked toasts

---
+++ src/app/core/services/task-toast.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class TaskToastService {
+    show = signal(false);
+    message = signal('');
+
+    showToast(message: string): void {
+        this.message.set(message);
+        this.show.set(true);
+        setTimeout(() => this.show.set(false), 2500);
+    }
+
+    taskCreated(): void {
+        this.showToast('Task edit to Board');
+    }
+
+    taskDeleted(): void {
+        this.showToast('Task deleted');
+    }
+
+    taskLocked(): void {
+        this.showToast("Task can't be deleted or edited");
+    }
+}
