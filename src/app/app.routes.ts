8dc99d843b7a70efbe0a1c3f2b5bea6ca3228fe9
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-09-01 15:22:14 +0200

    Feature/login layout (#61)
    * feat: add responsive login and sign-up layout with animations
    
    * fix: login layout figma conform
    
    * fix: center tablet layout, add password visibility

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { Help } from './pages/help/help';
 import { Policy } from './pages/policy/policy';
 import { Notice } from './pages/notice/notice';
 import { Board } from './components/board/board';
+import { Login } from './components/login/login';
 
 export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
     {
         path: '',
         component: MainLayout,
         children: [
-            { path: '', pathMatch: 'full', redirectTo: 'contacts' },
             { path: 'add-task', component: AddTask },
             { path: 'contacts', component: Contacts },
             { path: 'board', component: Board },
@@ -21,5 +23,5 @@ export const routes: Routes = [
             { path: 'legal-notice', component: Notice },
         ],
     },
-    { path: '**', redirectTo: 'contacts' },
+    { path: '**', redirectTo: 'login' },
 ];
