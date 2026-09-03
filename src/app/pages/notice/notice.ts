79aff56fae47b2e2303914f1836b06f050127aba
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-09-03 16:25:41 +0200

    Fix/mobile public legal pages (#72)
    * fix: remove duplicate greeting media query declaration
    
    * fix: align mobile public legal pages

--- src/app/pages/notice/notice.ts
+++ src/app/pages/notice/notice.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
     selector: 'app-notice',
     standalone: true,
+    imports: [RouterLink],
     templateUrl: './notice.html',
     styleUrl: './notice.scss',
 })
-export class Notice {}
+export class Notice {
+    protected readonly isLoggedIn = inject(AuthService).isLoggedIn;
+}
