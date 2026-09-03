3b507c1bad7bf45ac068b496e06864fc7e962f35
Ahmad Ataya <ahmadataya1980@gmail.com>
2026-09-03 16:17:19 +0200

    fix: align mobile public legal pages

--- src/app/pages/policy/policy.ts
+++ src/app/pages/policy/policy.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
     selector: 'app-policy',
     standalone: true,
+    imports: [RouterLink],
     templateUrl: './policy.html',
     styleUrl: './policy.scss',
 })
-export class Policy {}
+export class Policy {
+    protected readonly isLoggedIn = inject(AuthService).isLoggedIn;
+}
